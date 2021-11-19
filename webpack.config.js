051bbbb0e950789abd2c80baaf83866bce88d246
Ahn-Gihwan <ahngh0113@naver.com>
2021-11-19 16:15:09 +0900

    config: react-wepback-template

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { from } = require("webpack-sources/lib/CompatSource");
+
+module.exports = (env, option) => {
+  return {
+    entry: "./src/main.js",
+    output: {
+      filename: "main.js",
+    },
+    resolve: {
+      extensions: [".js", ".jsx"],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/i,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env", "@babel/preset-react"],
+            },
+          },
+        },
+        {
+          test: /\.css/,
+          use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+      }),
+      new MiniCssExtractPlugin({
+        filename: "style.css",
+      }),
+      new CopyPlugin({
+        patterns: [{ from: "static" }],
+      }),
+    ],
+  };
+};
